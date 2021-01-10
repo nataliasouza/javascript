e213f9be4b2f0380eb24a1aec5473b5117357943
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-10 19:17:03 -0300

    modifiquei o arquivo, inclui um novo exercício de FOR

--- repeticao.js
+++ repeticao.js
@@ -1,7 +1,12 @@
 var numeroSorteado = 10;
 
-var tabuada = 7;
+/*var tabuada = 7;
 
 for (var i = 0; i <= 10; i++) {
     console.log('Valor de ' + tabuada + 'x' + i + '=' + tabuada *i);
+}*/
+for (var i = 0; i < 100; i++) {
+    if (numeroSorteado === i) {
+        console.log('Seu numero foi encontrado');
+    }
 }
