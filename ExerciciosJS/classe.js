8203f04f53ac7aa00edc205e607c8c59e013d836
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-11 21:14:30 -0300

    Criei o arquivo de classe

--- ExerciciosJS/classe.js
+++ ExerciciosJS/classe.js
@@ -2,4 +2,14 @@ class matematica {
     soma (valorA, valorB) {
         return valorA + valorB;
     }
+    subtracao (valorA, valorB) {
+        return valorA - valorB;
+    }
 }
+
+var instaciaMatematica = new matematica();
+var resultado = instaciaMatematica.soma(3,10);
+var resultadoSub = instaciaMatematica.subtracao(10,7);
+
+console.log(resultado);
+console.log(resultadoSub);
